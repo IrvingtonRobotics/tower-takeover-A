dbe24b1082a7a5e252e513621d97c60373e8b664
Jared Hughes <jahughes241@gmail.com>
2019-12-30 13:08:31 -0800

    Move rail forward more and add target height for arms when rails forward

--- src/lift.cpp
+++ src/lift.cpp
@@ -39,12 +39,12 @@ class Lift {
   const QLength SMALL_MOVE_TOLERANCE = 0.2_in;
   // direction currently (or last) moved in for small movements
   int smallMoveDir = 1;
-  static const int NUM_HEIGHTS = 4;
   // give up on lowering to button after this time if not hit limit switch
   const QTime LOWER_TO_BUTTON_TIMEOUT = 5_s;
   const int DEFAULT_LOWER_SPEED = 50;
+  static const int NUM_HEIGHTS = 5;
   // WARNING: targetHeights MUST be sorted
-  const QLength targetHeights[NUM_HEIGHTS] = {MIN_ARM_HEIGHT, 16_in, 24.5_in, MAX_ARM_HEIGHT};
+  const QLength targetHeights[NUM_HEIGHTS] = {MIN_ARM_HEIGHT, MIN_ARM_HEIGHT+5_in, 16_in, 24.5_in, MAX_ARM_HEIGHT};
   const QLength MID_HEIGHT = (targetHeights[NUM_HEIGHTS-1] + targetHeights[0])/2;
   const int PORT = -LIFT_PORT;
   // is this currently doing a hard stop?
