fa0f809d2dd15f5968b1a30e8065b9e9d59e078c
Jared Hughes <jahughes241@gmail.com>
2020-02-02 00:30:29 -0800

    Deal with isolated matrix transform from Figma export in Auto Profile

--- utils/auto-profile/generate_motion_profile.py
+++ utils/auto-profile/generate_motion_profile.py
@@ -9,22 +9,30 @@ def generate(body, scale):
     return out_format(groups)
 
 def solve_groups(body, scale):
-    matches = re.findall(r'(<circle cx="(\d+(?:\.\d+)?)" cy="(\d+(?:\.\d+)?)[^>]*?fill-opacity="0.98"[^>]*/>)', body)
+    matches = re.findall(r'<circle [^>]*?fill-opacity="0.98"[^>]*/>', body)
     points = []
     points_complex = []
     angles = []
-    for match in matches:
-        full, x_s, y_s = match
-        x = float(x_s)/scale
-        y = float(y_s)/scale
+    float_re = r'-?\d+(?:\.\d+)?'
+    for full in matches:
+        matrix_match = re.search(fr'matrix\(((?:{float_re} ){{5}}{float_re})\)', full)
+        if matrix_match:
+          parts = [float(s) for s in matrix_match.group(1).split(" ")]
+          theta = math.acos(parts[0]) * 180/math.pi
+          x = parts[4]
+          y = parts[5]
+        else:
+          x_match = re.search(fr'cx="({float_re})"', full)
+          x = float(x_match.group(1)) if x_match else 0
+          y_match = re.search(fr'cy="({float_re})"', full)
+          y = float(y_match.group(1)) if y_match else 0
+          theta_match = re.search(fr'rotate\(({float_re})\)', full)
+          theta = float(theta_match.group(1)) if theta_match else 0
+        x /= scale
+        y /= scale
         points.append((x, y))
         pos = complex(f"{x}+{y}j")
         points_complex.append(pos)
-        theta_match = re.search(r'rotate\((-?\d+(?:\.\d+)?)[^>]*', full)
-        if theta_match:
-            theta = float(theta_match.group(1))
-        else:
-            theta = 0
         angles.append(theta)
     return groups(points, points_complex, angles)
 
@@ -46,13 +54,16 @@ def groups(points, points_complex, angles):
     add_pos(points[-1], angles[-1])
     return groups
 
+def f(x):
+  return round(x, 4)
+
 def out_format(groups):
     indent = "  "
     s = ""
     for group in groups:
         reversed, positions = group
         m = 1 if reversed else -1
-        points = map(lambda x: indent + f"Point{{{x[0]}_in, {m*x[1]}_in, {m*x[2]}_deg}}", positions)
+        points = map(lambda x: indent + f"Point{{{f(x[0])}_in, {f(m*x[1])}_in, {f(m*x[2])}_deg}}", positions)
         points_str = ",\n".join(points)
         s += "travelProfile({\n" + points_str + "\n}, " + ("true" if reversed else "false") + ", 0.5);\n"
     return s
