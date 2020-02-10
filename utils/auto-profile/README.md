6bf44004113b9134b5c142ffd21462c9806ec68a
Jared Hughes <jahughes241@gmail.com>
2020-02-10 00:19:58 -0800

    Use text boxes to specify auton code directly in Figma

--- utils/auto-profile/README.md
+++ utils/auto-profile/README.md
@@ -1,10 +1,27 @@
 ## Generate Motion Profile from SVG
 
-As a convenience for generating a motion profile for autonomous, you can edit an SVG (through Figma) to specify points. Any circle with opacity 0.98 specifies one point on the path, with points earlier in the SVG file specifying points earlier in time.
+As a convenience for generating a path for autonomous, you can edit an SVG (through Figma) to specify points. Any circle with opacity 0.98 specifies one point on the path, with points later in the SVG file (higher in Figma) specifying points earlier in time.
 
 In order to use Figma to edit this profile, it is convenient to create a component representing the robot with a circle in the center. Then to specify a new point on the path, duplicate the component and move/rotate as necessary.
 
-For Figma, note that the components should seem to be in REVERSE order. The starting position should be at the bottom (when looking at the sidebar on the left) and the ending position should be at the top. To export, select the frame, then Export as SVG in the bottom right.
+To actually move, you need to specify macros through text boxes in Figma. Create a text box anywhere in-frame, and reduce opacity to 0.01% or something so it is invisible to humans but not the SVG. Name the text box "#forward \<speed\>" such as "#forward 0.21" based on the following command table:
+
+| Macro | Description |
+|------------|----------|
+| `#forward <speed>` | Move forward |
+| `#back <speed>` | Move backward |
+| `#turn` | Turn in place |
+| `#suck` | Suck intake at full speed |
+
+The movements work on a stack system: Each robot in the sidebar adds a position/angle to the path, then a move macro moves between these positions. They always assume movement starting at the first position in the stack and ending at the last position, so the last position in one stack is automatically the first position in the next stack.
+
+Other fragments of code can be included as text boxes and are copied verbatim to the output code. Supported lines are those ending in a semicolon `;` or starting with `//` for a comment. Thus a text box named `intake.stop();` would be copied, as would `// finish stacking` or `lift.move(1); // small tower`, but other formats of text are ignored.
+
+Example:
+
+![](FigmaExample.png)
+
+To export, select the frame, then <kbd>Export as SVG</kbd> in the bottom right. Make sure to check the "Include 'id' Attribute" option, and you may like to uncheck "Outline Text" to avoid vectoring the text inside text elements.
 
 Once the SVG is downloaded, run
 
@@ -12,4 +29,4 @@ Once the SVG is downloaded, run
 
 e.g.
 
-    python3 auto-profile.py "Idea 1.svg" -s 2"
+    python3 auto-profile.py "Idea 1.svg" -s 2
--- utils/auto-profile/generate_motion_profile.py
+++ utils/auto-profile/generate_motion_profile.py
@@ -3,88 +3,143 @@
 import re
 import math
 import cmath
+from enum import Enum, auto
+from functools import reduce
+
+INDENT = "  "
+DEFAULT_SPEED = 0.20
+DISTANCE_TOL = 0.01
+
+class Tag(Enum):
+  POSITION = auto()
+  MACRO = auto()
+  VERBATIM = auto()
+
+class Position:
+  def __init__(self, start, x, y, theta):
+    self.type = Tag.POSITION
+    self.start = start
+    self.x = x
+    self.y = y
+    self.theta = theta
+
+  def dist(self, other):
+    return ((self.x-other.x)**2 + (self.y-other.y)**2)**0.5
+
+  def __str__(self):
+    return f"Position({self.x}, {self.y}, {self.theta})"
+
+class Macro:
+  def __init__(self, start, command, parameters):
+    self.type = Tag.MACRO
+    self.start = start
+    self.command = command
+    self.parameters = parameters
+
+  def run(self, positions):
+    params = self.parameters
+    if self.command == "suck":
+      assert len(positions) <= 1 and "Should not have a position stack on suck"
+      assert len(params) == 0 and "Suck speed not implemented"
+      yield "intake.move(QUICK_SUCK_SPEED);"
+    elif self.command in ["forward", "back"]:
+      assert len(positions) > 1 and "Should move between several points"
+      assert len(params) <= 1 and "Too many arguments for movement operation"
+      speed = float(params[0]) if len(params) == 1 else DEFAULT_SPEED
+      for i in range(len(positions)-1):
+        assert positions[i].dist(positions[i+1]) > DISTANCE_TOL and "Should have a meaningful distance between moves"
+      reversed = self.command != "forward"
+      yield "travelProfile({"
+      m = 1 if reversed else -1
+      for pos in positions:
+        yield INDENT + f"Point{{{f(pos.x)}_in, {f(m*pos.y)}_in, {f(m*pos.theta)}_deg}}"
+      yield "}, " + ("true" if reversed else "false") + ", " + f(speed) + ");"
+    elif self.command == "turn":
+      assert len(positions) == 2 and "Need two points to turn between"
+      assert len(params) == 0 and "Turn speed not implemented"
+      yield f"drive.turnAngle({f(positions[1].theta - positions[0].theta)}_deg);"
+    else:
+      assert False, f"Tag '{self.command}' not implemented"
+
+  def __str__(self):
+    return f"Macro({self.command}, {self.params})"
+
+class Verbatim:
+  def __init__(self, start, text):
+    self.type = Tag.VERBATIM
+    self.start = start
+    self.text = text
+
+  def __str__(self):
+    return f"Verbatim({self.text})"
 
 def generate(body, scale):
-    groups = solve_groups(body, scale)
-    return out_format(groups)
+  tags = []
+  tags += positions(body, scale)
+  tags += macros(body)
+  tags += verbatims(body)
+  tags.sort(key=lambda e: e.start, reverse=True)
+  return out_format(tags)
 
-def solve_groups(body, scale):
-    matches = re.findall(r'<circle [^>]*?fill-opacity="0.98"[^>]*/>', body)
-    points = []
-    points_complex = []
-    angles = []
-    float_re = r'-?\d+(?:\.\d+)?'
-    for full in matches:
-        matrix_match = re.search(fr'matrix\(((?:{float_re} ){{5}}{float_re})\)', full)
-        if matrix_match:
-          parts = [float(s) for s in matrix_match.group(1).split(" ")]
-          # range of acos from 0 to +pi
-          theta = math.acos(parts[0]) * 180/math.pi
-          if parts[1] < 0:
-            # negative sin, so mirror to negative range
-            theta = -theta
-          x = parts[4]
-          y = parts[5]
-        else:
-          x_match = re.search(fr'cx="({float_re})"', full)
-          x = float(x_match.group(1)) if x_match else 0
-          y_match = re.search(fr'cy="({float_re})"', full)
-          y = float(y_match.group(1)) if y_match else 0
-          # no close paren because a centerpoint is indicated on rotate
-          theta_match = re.search(fr'rotate\(({float_re})', full)
-          theta = float(theta_match.group(1)) if theta_match else 0
-        x /= scale
-        y /= scale
-        points.append((x, y))
-        pos = complex(f"{x}+{y}j")
-        points_complex.append(pos)
-        angles.append(theta)
-    return groups(points, points_complex, angles)
+def positions(body, scale):
+  matches = re.finditer(r'<circle [^>]*?fill-opacity="0.98"[^>]*/>', body)
+  float_re = r'-?\d+(?:\.\d+)?'
+  for match in matches:
+    full = match.group(0)
+    matrix_match = re.search(fr'matrix\(((?:{float_re} ){{5}}{float_re})\)', full)
+    if matrix_match:
+      parts = [float(s) for s in matrix_match.group(1).split(" ")]
+      # range of acos from 0 to +pi
+      theta = math.acos(parts[0]) * 180/math.pi
+      if parts[1] < 0:
+        # negative sin, so mirror to negative range
+        theta = -theta
+      x = parts[4]
+      y = parts[5]
+    else:
+      x_match = re.search(fr'cx="({float_re})"', full)
+      x = float(x_match.group(1)) if x_match else 0
+      y_match = re.search(fr'cy="({float_re})"', full)
+      y = float(y_match.group(1)) if y_match else 0
+      # no close paren because a centerpoint is indicated on rotate
+      theta_match = re.search(fr'rotate\(({float_re})', full)
+      theta = float(theta_match.group(1)) if theta_match else 0
+    x /= scale
+    y /= scale
+    yield Position(match.start(), x, y, theta)
 
-def similar_point(p1, p2):
-    tol = 0.01
-    return abs(p2[0]-p1[0]) < tol and abs(p2[1]-p1[1]) < tol
+def macros(body):
+  matches = re.finditer(r'<text id="#([^"]*?)(_\d*)?"', body);
+  for match in matches:
+    s = match.group(1).split(" ")
+    command = s[0]
+    params = s[1:]
+    yield Macro(match.start(), command, params)
 
-
-def groups(points, points_complex, angles):
-    groups = [(False, [])]
-    def add_pos(point, angle):
-        groups[-1][1].append((*point, angle))
-
-    reversing = False
-    for pt, point, angle, next_point in zip(points, points_complex, angles, points_complex[1:]):
-        if len(groups[-1][1]) > 0 and similar_point(groups[-1][1][-1], pt):
-          groups.append((\
-            "intake.stop();\n"
-            f"drive.turnAngle({f(angle - groups[-1][1][-1][-1])}_deg);\n"
-            "intake.move(QUICK_SUCK_SPEED);",))
-          groups.append((reversing, []))
-          add_pos(pt, angle)
-        else:
-          add_pos(pt, angle)
-        move_angle_degrees = cmath.phase(next_point - point) * 180/math.pi
-        diff = move_angle_degrees - angle
-        # regret not using dotproduct
-        diff %= 360
-        if (90 < diff < 270) ^ reversing:
-            reversing = not reversing
-            groups.append((reversing, [groups[-1][1][-1]]))
-    add_pos(points[-1], angles[-1])
-    return groups
+def verbatims(body):
+  # either comments or `;` terminated statements or combos, not starting with "#" (macro character)
+  matches = re.finditer(r'<text id="((?!#)([^"]*?;)?\s*(// [^"]*?)?)(_\d*)?"', body)
+  for match in matches:
+    s = match.group(1)
+    if s != "":
+      yield Verbatim(match.start(), s)
 
 def f(x):
-  return round(x, 4)
+  return str(round(x, 4))
 
-def out_format(groups):
-    indent = "  "
-    s = ""
-    for group in groups:
-        if len(group) == 1:
-          s += group[0] + "\n"
-          continue
-        reversed, positions = group
-        m = 1 if reversed else -1
-        points = map(lambda x: indent + f"Point{{{f(x[0])}_in, {f(m*x[1])}_in, {f(m*x[2])}_deg}}", positions)
-        points_str = ",\n".join(points)
-        s += "travelProfile({\n" + points_str + "\n}, " + ("true" if reversed else "false") + ", 0.5);\n"
-    return s
+def out_format(tags):
+  lines = []
+  positions = []
+  for tag in tags:
+    if tag.type == Tag.POSITION:
+      positions.append(tag)
+    elif tag.type == Tag.MACRO:
+      lines += tag.run(positions)
+      # positions consumed, but keep the last (new position)
+      if len(positions) > 0:
+        positions = [positions[-1]]
+    elif tag.type == Tag.VERBATIM:
+      lines.append(tag.text)
+    else:
+      assert False, "Unaccepted tag"
+  return "\n".join(lines)
